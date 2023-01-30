8623211833a648fad99a82976890aceacbd2b79a
Eroha <erohazakurdaev@yandex.ru>
2023-01-30 14:11:42 +0300

    Спринт 5. Итерация 2.

--- src/logics/InMemoryHistoryManager.java
+++ src/logics/InMemoryHistoryManager.java
@@ -1,7 +1,6 @@
 package logics;
 
 import data.Task;
-import data.Node;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,17 +8,15 @@ import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
     private final HashMap<Integer, Node> historyMap = new HashMap<>();
-    CustomLinkedList customLinkedList = new CustomLinkedList();
+    private final CustomLinkedList customLinkedList = new CustomLinkedList();
 
     @Override
     public void add(Task task) {
         if (historyMap.containsKey(task.getId())) {
             customLinkedList.removeNode(historyMap.get(task.getId()));
             historyMap.remove(task.getId());
-            historyMap.put(task.getId(), customLinkedList.linkLast(task));
-        } else {
-            historyMap.put(task.getId(), customLinkedList.linkLast(task));
         }
+        historyMap.put(task.getId(), customLinkedList.linkLast(task));
     }
 
     @Override
@@ -34,58 +31,60 @@ public class InMemoryHistoryManager implements HistoryManager {
             historyMap.remove(id);
         }
     }
-}
 
-class CustomLinkedList {
-    public Node first;
-    public Node last;
-    private int size = 0;
 
-    public Node linkLast(Task task) {
-        Node newNode = new Node(task);
-        if (first == null) {
-            newNode.next = null;
-            newNode.previous = null;
-            first = newNode;
-            last = newNode;
-        } else {
-            last.next = newNode;
-            newNode.previous = last; //ссылаем новый узел на предыдущий
-            last = newNode; // последним становится новый
+    private static class CustomLinkedList {
+        public Node first;
+        public Node last;
+        private int size = 0;
+
+        public Node linkLast(Task task) {
+            Node newNode = new Node(task);
+            if (first == null) {
+                newNode.setNext(null);
+                newNode.setPrevious(null);
+                first = newNode;
+                last = newNode;
+            } else {
+                last.setNext(newNode);
+                newNode.setPrevious(last); //ссылаем новый узел на предыдущий
+                last = newNode; // последним становится новый
+            }
+            size++;
+            return newNode;
         }
-        size++;
-        return newNode;
-    }
 
-    public ArrayList<Task> getTasks() {
-        ArrayList<Task> data = new ArrayList<>();
-        Node returnTask = first;
-        for (int i = 0; i < size; i++) {
-            data.add(returnTask.data);
-            returnTask = returnTask.next;
+        public ArrayList<Task> getTasks() {
+            ArrayList<Task> data = new ArrayList<>();
+            Node returnTask = first;
+            while (returnTask != null){
+                data.add(returnTask.getData());
+                returnTask = returnTask.getNext();
+            }
+            return data;
         }
-        return data;
-    }
 
-    public void removeNode(Node node) {
-        if (node.previous == null && node.next == null) {
-            first = null;
-            last = null;
-            size--;
-        } else if (node.previous == null) {
-            node.next.previous = null;
-            first = node.next;
-            size--;
-        } else if (node.next == null) {
-            node.previous.next = null;
-            last = node.previous;
-            size--;
-        } else {
-            node.previous.next = node.next;
-            node.next.previous = node.previous;
-            node.next = node.previous = null;
-            node.data = null;
-            size--;
+        public void removeNode(Node node) {
+            if (node.getPrevious() == null && node.getNext() == null) {
+                first = null;
+                last = null;
+                size--;
+            } else if (node.getPrevious() == null) {
+                node.getNext().setPrevious(null);
+                first = node.getNext();
+                size--;
+            } else if (node.getNext() == null) {
+                node.getPrevious().setNext(null);
+                last = node.getPrevious();
+                size--;
+            } else {
+                node.getPrevious().setNext(node.getNext());
+                node.getNext().setPrevious(node.getPrevious());
+                node.setNext(null);
+                node.setPrevious(null);
+                node.setData(null);
+                size--;
+            }
         }
     }
 }
---
+++ src/logics/Node.java
@@ -0,0 +1,38 @@
+package logics;
+
+import data.Task;
+
+public class Node {
+
+    private Task data;
+    private Node next;
+    private Node previous;
+
+    public Node(Task data) {
+        this.data = data;
+    }
+
+    public Task getData() {
+        return data;
+    }
+
+    public void setData(Task data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(Node previous) {
+        this.previous = previous;
+    }
+}
